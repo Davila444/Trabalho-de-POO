8ccbfbc30a57dc2e8315c28427a4b5e4c9e4c828
Davila Almeida <davilaalmeida909@gmail.com>
2026-05-30 17:32:11 -0300

    tirando as interfaces de carrinho e pedido, pois só haverá um tipo de carrinho e um tipo de pedido(Se eu tiver entendido o conceito de interface corretamente)

--- interfaces.java
+++ interfaces.java
@@ -1,16 +1,6 @@
 
 // INTERFACES DO SISTEMA
 
-public interface ICarrinho {
-    void adicionarItem();
-    void removerItem();
-    void calcularTotal();
-}
-
-public interface IPedido {
-    void gerarPedido();
-    void calcularTotal();
-}
 
 public interface IPagamento {
     void processandoPagamento();
