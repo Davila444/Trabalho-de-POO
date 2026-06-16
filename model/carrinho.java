823e68f15b374b930db80032556898ed315d94c1
moiseslovegod <moiseslovegod@gmail.com>
2026-06-16 16:07:39 -0300

    criada a classe carrinho e seus metodos

---
+++ model/carrinho.java
@@ -0,0 +1,26 @@
+package
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class carrinho implements FazerCalculo {
+    private List<ItemCarrinho> itens;
+
+    public Carrinho() {
+        this.itens = new ArrayList<>();
+
+        public Carrinho() {
+            this.itens = new ArrayList();
+        }
+        
+        public void adicionarItem(ItemCarrinho item) {
+            this.itens.add(item);
+        }
+
+        public void removerItem(ItemCarrinho item) {
+            this.itens.remove(Item);
+        }
+        @Override
+        public double
+    }
+}
