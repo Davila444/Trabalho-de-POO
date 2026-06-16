63f177cede240afef95cf6ad506d53e6faed53d5
moiseslovegod <moiseslovegod@gmail.com>
2026-06-16 20:01:27 -0300

    organizei os packages para despois separalos e importalos pras classes certas

--- model/Produto.java
+++ model/Produto.java
@@ -1,5 +1,5 @@
 package model;
-abstract class Produto {
+public abstract class Produto {
     private String id;
     private String nome;
     private double preco;
--- model/carrinho.java
+++ model/carrinho.java
@@ -3,34 +3,31 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class carrinho implements FazerCalculo {
+public class Carrinho implements FazerCalculo {
 
     private List<ItemCarrinho> itens;
 
     public Carrinho() {
-        this.itens = new ArrayList<>() {
-
-        }
-    
+        this.itens = new ArrayList<ItemCarrinho>();
+    }
         public void adicionarItem(ItemCarrinho item) {
             this.itens.add(item);
-        }
+    }
 
         public void removerItem(ItemCarrinho item) {
             this.itens.remove(item);
-        }
+    }
         @Override
         public double calcularTotal() {
             double total = 0.0;
             
-
             for (ItemCarrinho item : itens) {
                 total += item.getQuantidade() * item.getProduto().getPreco();
-            }
+        }
+
             return total;
         }
         public List<ItemCarrinho> getItens() {
             return itens;
         }
-    }
 }
