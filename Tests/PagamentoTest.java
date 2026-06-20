e80b019f5017c69564236db404f8873303986f57
Davila Almeida <davilaalmeida909@gmail.com>
2026-06-20 00:02:15 -0300

    teste de pagamento

--- Tests/PagamentoTest.java
+++ Tests/PagamentoTest.java
@@ -1,5 +1,24 @@
 package Tests;
 
-public class PagamentoTest {
-    
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import controller.Pagamento;
+
+
+ class PagamentoTest {
+    private Pagamento pagamento;
+
+    @BeforeEach
+    void setup(){
+        pagamento = new Pagamento("1", "pix", "Pendente");
+    }
+
+    @Test
+    void testprocessarPagamento(){
+        pagamento.processandoPagamento();
+        assertEquals("Aprovado", pagamento.getStatus());
+    }
+
+
 }
