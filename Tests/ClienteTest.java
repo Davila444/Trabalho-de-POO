ba0e23e412d70354ff3ae7efd3c1083de1772b37
Davila Almeida <davilaalmeida909@gmail.com>
2026-07-01 17:59:48 -0300

    os testes relacionados a cliente

--- Tests/ClienteTest.java
+++ Tests/ClienteTest.java
@@ -1,5 +1,23 @@
 package Tests;
 
-public class ClienteTest {
-    
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import model.Cliente;
+import model.Pedido;
+
+class ClienteTest {
+
+    private Cliente cliente;
+
+    @BeforeEach
+    void setup() {
+        cliente = new Cliente("Jonas", "Jonas@email.com", "12345", "Rua B");
+    }
+
+    @Test
+    void testCarrinhoNaoNulo() {
+        assertNotNull(cliente.getCarrinho());
+    }
+
 }
