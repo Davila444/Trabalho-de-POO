701eec3e7347f136683af17fef234d3898e38466
Davila Almeida <davilaalmeida909@gmail.com>
2026-07-01 18:53:30 -0300

    testes relacionados a produtos

--- Tests/ProdutoTest.java
+++ Tests/ProdutoTest.java
@@ -1,5 +1,25 @@
 package Tests;
 
-public class ProdutoTest {
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import model.Produto;
+import excessoes.QuantidadeInvalidaException;
+
+class ProdutoTest {
+
+    class ProdutoTeste extends Produto {
+        public ProdutoTeste(String id, String nome, double preco, int estoque) {
+            super(id, nome, preco, estoque);
+        }
+    }
+
+    private Produto produto;
+
+    @BeforeEach
+    void setup() {
+        produto = new ProdutoTeste("1", "Notebook", 2500, 10);
+    }
+
     
 }
